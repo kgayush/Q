7cdc7fd70408a88bfe87f54891b2dd4613a759c4
agga1211 <54178133+agga1211@users.noreply.github.com>
2022-02-19 23:41:50 +0530

    Q3 Added

--- Python/Operators/Q3.py
+++ Python/Operators/Q3.py
@@ -1,8 +1,8 @@
 from datetime import date
 
-date1 = date(2022, 2, 19)
-date2 = date(2021, 4, 19)
+date1 = date(2014, 7, 2)
+date2 = date(2014, 7, 11)
 
 num_of_days = abs((date2 - date1).days)
 
-print(f'Number of days: {num_of_days}')
+print(f'{num_of_days} days')
