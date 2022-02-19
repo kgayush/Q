ac33aa41b72f822c34e446f50e2e12ae77773abd
Ayush Gupta <77568637+kgayush@users.noreply.github.com>
2022-02-19 23:12:36 +0530

    Delete Q2.py

--- Python/Q2.py
+++
@@ -1,8 +0,0 @@
-length = int(input("Enter length of list: "))
-number_list = []
-
-for i in range(length):
-    number = int(input())
-    number_list.append(number)
-
-print(f'Maximum: {max(number_list)}')
